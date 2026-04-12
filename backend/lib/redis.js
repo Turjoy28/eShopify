89cef2889578933c48d7b26482b4e9b75070e789
Turjoy <saifbus28@gmail.com>
2026-04-13 00:23:15 +0600

    done

--- backend/lib/redis.js
+++ backend/lib/redis.js
@@ -5,7 +5,9 @@ dotenv.config();
 export const redis = new Redis(process.env.UPSTASH_REDIS_URL, {
   tls: {
     rejectUnauthorized: false
-  }
+  },
+  retryDelayOnFailover: 100,
+  maxRetriesPerRequest: 3,
+  lazyConnect: true
 });
-await redis.set('fool', 'bar');
 
